6b0051d3c35da14ba772670d6c1017b36c53ece1
hkmsoft <hkmcss@gmail.com>
2022-08-22 22:07:09 +0800

    Keep JsonView open state

--- src/ui/jsonview/JsonViewer.tsx
+++ src/ui/jsonview/JsonViewer.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import "./JsonViewer.less";
 
+interface JsonViewState {
+    [key: string]: boolean
+}
+
 interface JsonViewerProps {
     data: any
 }
 export function JsonViewer(props: JsonViewerProps) {
+    const viewState: JsonViewState = {};
+
     return (
         <div className="jsonViewer">
             <table cellPadding={0} cellSpacing={0}>
@@ -15,7 +21,7 @@ export function JsonViewer(props: JsonViewerProps) {
                     </tr>
                 </thead>
                 <tbody>
-                    <JsonViewerNodes parentName="" data={props.data} level={0} />
+                    <JsonViewerNodes parentName="" data={props.data} level={0} jsonViewState={viewState} />
                 </tbody>
             </table>
         </div>
@@ -27,7 +33,8 @@ export function JsonViewer(props: JsonViewerProps) {
 interface JsonViewerNodeProps {
     parentName: string,
     data: any,
-    level: number
+    level: number,
+    jsonViewState: JsonViewState
 }
 
 function JsonViewerNodes(props: JsonViewerNodeProps) {
@@ -36,12 +43,11 @@ function JsonViewerNodes(props: JsonViewerNodeProps) {
         for (const key in props.data) {
             const viewId = `${props.parentName}/${key}`
             nodes.push(
-                <JsonViewerNodeView key={viewId} viewId={viewId} name={key} value={props.data[key]} level={props.level} />
-
+                <JsonViewerNodeView key={viewId} viewId={viewId} name={key} value={props.data[key]} level={props.level} jsonViewState={props.jsonViewState} />
             );
         }
     } else {
-        nodes.push(<JsonViewerNodeView key={props.parentName} viewId={props.parentName} name="" value={props.data} level={props.level} />);
+        nodes.push(<JsonViewerNodeView key={props.parentName} viewId={props.parentName} name="" value={props.data} level={props.level} jsonViewState={props.jsonViewState} />);
     }
     return (
         <>
@@ -55,10 +61,12 @@ interface JsonViewerNodeView {
     level: number,
     name: string | number,
     value: string | number | null | undefined,
+    jsonViewState: JsonViewState
 }
 
 function JsonViewerNodeView(props: JsonViewerNodeView) {
-    const [isOpened, setOpened] = useState<boolean>(false);
+    const [isOpened, setIsOpened] = useState(Boolean(props.jsonViewState[props.viewId]));
+    console.log(props.viewId, props.jsonViewState[props.viewId]);
 
     const paddingInlineStart = props.level * 16;
     let arrowClass = "";
@@ -87,18 +95,20 @@ function JsonViewerNodeView(props: JsonViewerNodeView) {
     }
 
     if (isOpened && hasNextLevel) {
-        nextLevelContent = <JsonViewerNodes parentName="" data={props.value} level={props.level + 1} />
+        nextLevelContent = <JsonViewerNodes parentName={props.viewId} data={props.value} level={props.level + 1} jsonViewState={props.jsonViewState} />
     }
 
     function toggleNode() {
-        if (hasNextLevel)
-            setOpened(!isOpened);
+        if (hasNextLevel){
+            props.jsonViewState[props.viewId] = !isOpened;
+            setIsOpened(props.jsonViewState[props.viewId]);
+        }
     }
 
     return (
         <>
             <tr key={`${props.viewId}-node`} className={`jsonViewerNode jsonView-node-${props.level} ${isOpened ? "jsonViewerNode-opened" : ""}`}>
-                <td className="jsonViewerNode-label" style={{paddingInlineStart: `${paddingInlineStart}px`}} onClick={toggleNode}>
+                <td className="jsonViewerNode-label" style={{ paddingInlineStart: `${paddingInlineStart}px` }} onClick={toggleNode}>
                     <span className={"jsonViewerNode-icon " + arrowClass}></span>
                     <span className="jsonViewerNode-labelText">
                         {props.name}
