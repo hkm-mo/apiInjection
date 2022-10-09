2ecbff5c2348a04f752ca01dacb89ad7e640da85
hkmsoft <hkmcss@gmail.com>
2022-10-09 20:11:25 +0800

    Fix missing useTopLayerPortal

--- src/ui/uiUtilities.ts
+++ src/ui/uiUtilities.ts
@@ -1,5 +1,24 @@
+import { useEffect, useLayoutEffect, useState } from "react";
 
 let localIdSeed = 0;
 export function getLocalId() {
     return localIdSeed++;
+}
+
+
+let portalZIndex = 1000;
+export function useTopLayerPortal() {
+    const [root,] = useState(() => {
+        const elm = document.createElement('div');
+        elm.className = "portalRoot";
+        elm.style.setProperty('--portal-z-index', String(portalZIndex++));
+        return elm;
+    });
+
+    useLayoutEffect(() => {
+        document.body.appendChild(root);
+        return () => root.remove();
+    }, [root]);
+
+    return root;
 }
